a01e49b68255a599ca713ccd90110fd01add44c5
Erdoganishe <Khotyanmisha@gmail.com>
2023-05-02 16:38:13 +0300

    Index + script for main edited

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,70 +2,58 @@ window.onload = async function () {
   const dishes = [];
 
   //get food names
-  const response = await fetch("http://localhost:3500/food", {method: "GET"});// add "POST", "PUT"...
+  const response = await fetch("http://localhost:3500/food", { method: "GET" });// add "POST", "PUT"...
   const jsonData = await response.json();
   //console.log(jsonData);
-  jsonData.forEach(data => dishes.push({name: data.name, path: data.id}));
-
-  // set 12 first default 
+  jsonData.forEach(data => dishes.push({ name: data.name, path: data._id }));
+  console.log(dishes);
+  // get array of img on page
   const images = [];
   for (let i = 1; i <= 12; i++) {
     const image = document.querySelector(`img[alt="Image ${i}"]`);
     if (image) {
-      images.push(image.getAttribute("src"));
+      images.push(image);
+      image.src = `img/front_img/${dishes[i - 1].path}.jpg`;
     }
   }
 
-  const imageNumbers = images.map(image => Number(image.match(/\d+/)[0]));
+  //set default text
   const textBelowImages = document.querySelectorAll('.text-below-image');
   for (let i = 0; i < textBelowImages.length; i++) {
-    textBelowImages[i].innerHTML = dishes[imageNumbers[i] - 1].name;
+    textBelowImages[i].innerHTML = dishes[i].name;
   }
 
   const prevButton = document.getElementById('prev-button');
   const nextButton = document.getElementById('next-button');
 
+  let currentFirst = 0;
+  
+  //previous button press event
 
   prevButton.addEventListener('click', function () {
+    currentFirst = currentFirst-12+dishes.length;
+    currentFirst = currentFirst % dishes.length;
+
     for (let i = 0; i < 12; i++) {
 
-      let match = images[i].match(/img\/front_img\/(\d+)\.jpg/);
-      match = parseInt(match[1], 10);
-      match += dishes.length - 12;
-      match = match % dishes.length;
-      if (match == 0) { match = dishes.length; }
-      images[i] = `img/front_img/${match}.jpg`;
-      const image = document.querySelector(`img[alt="Image ${i + 1}"]`);
-      image.src = images[i];
+      images[i].src = `img/front_img/${dishes[currentFirst + i].path}.jpg`;
+      textBelowImages[i].innerHTML = dishes[currentFirst + i].name;
 
     }
-    const imageNumbers2 = images.map(image => Number(image.match(/\d+/)[0]));
-    for (let i = 0; i < textBelowImages.length; i++) {
-      textBelowImages[i].innerHTML = dishes[imageNumbers2[i] - 1].name;
-    }
-
-
   });
 
+  //next button press event
+
   nextButton.addEventListener('click', function () {
+    currentFirst = currentFirst+12;
+    currentFirst = currentFirst % dishes.length;
+
     for (let i = 0; i < 12; i++) {
 
-      let match = images[i].match(/img\/front_img\/(\d+)\.jpg/);
-      match = parseInt(match[1], 10);
-      match += 12;
-      match = match % dishes.length;
-      if (match == 0) { match = dishes.length; }
-      images[i] = `img/front_img/${match}.jpg`;
-      const image = document.querySelector(`img[alt="Image ${i + 1}"]`);
-      image.src = images[i];
+      images[i].src = `img/front_img/${dishes[currentFirst + i].path}.jpg`;
+      textBelowImages[i].innerHTML = dishes[currentFirst + i].name;
 
     }
-
-    const imageNumbers2 = images.map(image => Number(image.match(/\d+/)[0]));
-    for (let i = 0; i < textBelowImages.length; i++) {
-      textBelowImages[i].innerHTML = dishes[imageNumbers2[i] - 1].name;
-    }
-
   });
 
 };
