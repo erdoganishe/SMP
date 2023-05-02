8c56733f43e55eaf4d393dbdc754741e936aa28b
Erdoganishe <Khotyanmisha@gmail.com>
2023-05-02 21:04:55 +0300

    Recipe.js edited

--- public/scripts/recipe.js
+++ public/scripts/recipe.js
@@ -1,18 +1,11 @@
 window.onload = async function () {
+    const numberOfFood = '645032f291c28a8ab5d7ddd7';
     const numberOfRecipe = '645143264b759c5faf4eb1f3';
 
     // get data from db
-    const dataArray = [];
     const response = await fetch(`http://localhost:3500/api/recipe/${numberOfRecipe}`);// add "POST", "PUT"...
     const jsonData = await response.json();
     
-    //push data to arra CHANGE IT !!!!!!!!!!
-    dataArray.push(jsonData.difficulty);
-    dataArray.push(jsonData.time);
-    jsonData.steps.forEach(step => dataArray.push(step));
-
-    console.log(dataArray);
-
     const hardSpan = document.getElementById("hard-span");
     const timeSpan = document.getElementById("time-span");
     const paragraphs = document.getElementsByTagName("p");
@@ -20,16 +13,16 @@ window.onload = async function () {
     images[0].src = `img/receipt_db/${numberOfRecipe}/history.png`;
     
     // dont work(hm, why?)))) CHANGE IT !!!!!!!!!!
-    //images[1].src = `img/front_img/${numberOfRecipe}.jpg`;
+    images[1].src = `img/front_img/${numberOfFood}.jpg`;
 
     images[2].src = `img/receipt_db/${numberOfRecipe}/step.png`;
     
     // Задати внутрішній HTML для елементів з id
-    hardSpan.innerHTML = dataArray[0];
-    timeSpan.innerHTML = dataArray[1];
+    hardSpan.innerHTML = jsonData.difficulty;
+    timeSpan.innerHTML = jsonData.time;
 
     // Задати внутрішній HTML для елементів <p>
     for (let i = 0; i < paragraphs.length; i++) {
-        paragraphs[i].innerHTML = dataArray[i+2];
+        paragraphs[i].innerHTML = jsonData.steps[i];
     }
 };
