f1822e9184ebdc2ba1f49161a0c355c02b5a1a03
usiozero <swortaartonlineforever@gmai.com>
2023-05-04 15:18:25 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ middleware/fileExtLimiter.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const fileExtLimiter = (allowedExtArray) => {
+    return (req, res, next) => {
+        const files = req.files;
+
+        const fileExtens
+    }
+}
--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
 router.get('^/$|index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'index.html'));
@@ -10,9 +11,14 @@ router.get('/recipe(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'recipe.html'));
 });
 
-router.get('/newRecipe(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views', 'newRecipe.html'));
-});
+router.route('/newRecipe(.html)?')
+    .get((req, res) => {res.sendFile(path.join(__dirname, '../views', 'newRecipe.html'));})
+    .post(fileUpload({ createParentPath: true }), (req, res) => {
+        const files = req.files;
+        console.log(files);
+
+        return res.json({ status: "logged", message: "logged"});
+    });
 
 
 
