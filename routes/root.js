2a4a08a3fa1edf26f40cbbed58966d0315cf0503
Erdoganishe <Khotyanmisha@gmail.com>
2023-05-04 17:33:56 +0300

    LB3 last added

--- routes/root.js
+++ routes/root.js
@@ -14,6 +14,8 @@ router.get('/newRecipe(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'newRecipe.html'));
 });
 
-
+router.get('/roflpage(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'roflpage.html'));
+});
 
 module.exports = router;
