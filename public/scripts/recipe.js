04313bf0e1b5718c5c4b9e9d34a8b6a52de4f5f9
usiozero <swortaartonlineforever@gmai.com>
2023-05-05 15:06:18 +0200

    filr -> new fol

--- public/scripts/recipe.js
+++ public/scripts/recipe.js
@@ -1,7 +1,7 @@
 window.onload = async function () {
     //const numberOfFood = '645032f291c28a8ab5d7ddd7';
     //645032f291c28a8ab5d7ddd7
-    const numberOfRecipe = '645143264b759c5faf4eb1f3';
+    //const numberOfRecipe = '645143264b759c5faf4eb1f3';
     // Отримуємо значення параметру id з URL
     const searchParams = new URLSearchParams(window.location.search);
     const numberOfFood = searchParams.get('id');
@@ -19,12 +19,12 @@ window.onload = async function () {
     const timeSpan = document.getElementById("time-span");
     const paragraphs = document.getElementsByTagName("p");
     const images = document.getElementsByClassName("food-img");
-    images[0].src = `img/receipt_db/${numberOfRecipe}/history.png`;
+    images[0].src = `img/receipt_db/${numberOfFood}/history.png`;
 
     // dont work(hm, why?)))) CHANGE IT !!!!!!!!!!
     images[1].src = `img/front_img/${numberOfFood}.jpg`;
 
-    images[2].src = `img/receipt_db/${numberOfRecipe}/step.png`;
+    images[2].src = `img/receipt_db/${numberOfFood}/step.png`;
 
     // Задати внутрішній HTML для елементів з id
     hardSpan.innerHTML = jsonData.difficulty;
