d7e6150790b4a21fa379b48852335a6bcda8947c
Erdoganishe <Khotyanmisha@gmail.com>
2023-06-03 00:42:20 +0300

    script added

---
+++ public/scripts/chat.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', async function () {
+
+
+    var socket = io();
+
+    var messages = document.getElementById('messages');
+    var form = document.getElementById('form');
+    var input = document.getElementById('input');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (input.value) {
+            socket.emit('chat message', input.value);
+            input.value = '';
+        }
+    });
+
+    socket.on('chat message', function (msg) {
+        var item = document.createElement('li');
+        item.textContent = msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+
+    io.on('connection', (socket) => {
+        socket.on('chat message', msg => {
+            io.emit('chat message', msg);
+        });
+    });
+});
