0cea4ec2f341c5be903c310df9b274fbfc927e09
usiozero <swortaartonlineforever@gmai.com>
2023-06-02 23:30:26 +0200

    in s

--- middleware/initSocket.js
+++ middleware/initSocket.js
@@ -7,20 +7,20 @@ const initSocketIO = (server) => {
   io.on('connection', (socket) => {
     console.log('con');
     // Event when a player joins the room
-    socket.on('join', (roomId, playerId, isWhite) => {
+    socket.on('join', (roomId, playerId, ) => {
       socket.join(roomId); // Join the specified room
       socket.playerId = playerId; // Store the player ID in the socket
       console.log(`${roomId} - ${playerId}`);
       // Notify other players in the room that a new player has joined
 
-      socket.to(roomId).emit('playerJoined', playerId);
+      socket.to(roomId).emit('userJoined', playerId);
     });
 
     // Event when a player makes a move
-    socket.on('move', (roomId, dataArray, legalMovesForPlayer, timers) => {
+    socket.on('message', () => {
       // Emit the move event to other players in the room
       console.log(roomId);
-      socket.to(roomId).emit('opponentMove', dataArray, legalMovesForPlayer, timers);
+      socket.to(roomId).emit('opponentMessage', );
     });
 
     // Event when a player disconnects
