7f6ff992943ae19d90d170e59b1d69b49305798f
usiozero <swortaartonlineforever@gmai.com>
2023-06-02 23:12:49 +0200

    pre socket commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,10 @@
 const allowedOrigins = [
     'https://www.DOMAINNAME.com', // Change
     'http://127.0.0.1:3500',
+    '192.168.1.113',
+    '192.168.1.114',
+    'http://127.0.0.1:8005',
+    'http://127.0.0.1:8000',
     'http://localhost:3500'];
 
 module.exports = allowedOrigins;
