4669f41f93236be0c9bc2487b44125a59199ce16
usiozero <swortaartonlineforever@gmai.com>
2023-06-06 04:54:57 +0200

    al or to onrender

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,13 +1,15 @@
 const allowedOrigins = [
     'https://www.DOMAINNAME.com', // Change
     'http://127.0.0.1:3500',
-    '192.168.1.113',
-    '192.168.1.114',
-    "3.75.158.163",
-    "3.125.183.140",
-    "35.157.117.28",
+    // '192.168.1.113',
+    // '192.168.1.114',
+    // "3.75.158.163",
+    // "3.125.183.140",
+    // "35.157.117.28",
+    'https://food-dictionary.onrender.com',
     'http://127.0.0.1:8005',
     'http://127.0.0.1:8000',
-    'http://localhost:3500'];
+    'http://localhost:3500'
+];
 
 module.exports = allowedOrigins;
